4379e3c93ef7af6a4490476aef2215e756f0824a
TIFFANY MAINA <tiffmaina@gmail.com>
2023-09-28 11:39:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name : "alice",
+    streetAddress : "kikuyu",
+}
+console.log(employee);
+
+function  updateEmployeeWithKeyAndValue(employee, streetAddress, name){
+ const employee2 = {...employee, streetAddress : "11 Broadway", name : "Sam"};
+ return (employee2)
+}
+console.log(employee2);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,streetAddress){
+employee.streetAddress = '12 Broadway';
+return(employee);
+}
+
+function deleteFromEmployeeByKey(){
+    let newEmployee = {...employee}
+    delete newEmployee.name;
+    return (newEmployee);
+}
+function destructivelyDeleteFromEmployeeByKey(employee){
+    delete employee.name;
+    return(employee);    
+}
